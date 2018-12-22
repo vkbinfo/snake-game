ebede5a5350699315338cd7cd87d5ac774d39ad4
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2018-12-22 07:52:55 +0530

    Added food creation and food eating for snake

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+const GRID = [];
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    GRID.push([i,j]);
+  }
+}
+
 class App extends Component {
   state = {
-    snakeCells: [[15, 2], [15,1], [15,0]]
+    snakeCells: [[15, 2], [15,1], [15,0]],
+    foodPosition: [-1,-1]
   }
   direction = 'right';
   paused = false;
@@ -15,6 +23,8 @@ class App extends Component {
     this.startGame = this.startGame.bind(this);
     this.moveSnake = this.moveSnake.bind(this);
     this.pauseGame = this.pauseGame.bind(this);
+    this.createFood = this.createFood.bind(this);
+    this.hasEatenFood = this.hasEatenFood.bind(this);
     this.createFood();
   }
 
@@ -34,7 +44,7 @@ class App extends Component {
       //down
       this.direction = 'down';
     }
-    
+    this.moveSnake();
   }
 
   componentDidMount(){
@@ -43,10 +53,6 @@ class App extends Component {
 
   componentWillUnmount(){
     document.removeEventListener("keydown", this.keyFunction, false);
-  }
-
-  createFood() {
-
   }
 
   startGame() {
@@ -70,29 +76,52 @@ class App extends Component {
       snakeCells.splice(0, 0, [snakeHead[0] + 1, snakeHead[1]]);
     }
 
-    if ( this.hasEatenFood()) {
-
-    }
     if ( this.hasCollision(snakeCells)) {
       clearInterval(this.interval);
       alert('Your snake is not in your control');
       return;
     }
-    snakeCells.pop();
+    if (this.hasEatenFood()) {
+        this.createFood();
+    } else {
+      snakeCells.pop();
+    } 
     this.setState({snakeCells})
-    console.log(snakeCells)
+    
+  }
+
+  createFood() {
+    let gridWithoutSnakeCells = GRID.filter((gridCell) => {
+      let x = gridCell[0];
+      let y = gridCell[1];
+      let found = this.state.snakeCells.find((snakeCell) => {
+        return (x === snakeCell[0] && y === snakeCell[1])
+      })
+      if (found) {
+        return false;
+      }
+      return true;
+    });
+    let foodPosition = gridWithoutSnakeCells[Math.floor(Math.random()*gridWithoutSnakeCells.length)];
+    this.state.foodPosition = foodPosition;
   }
 
   hasEatenFood() {
-
+    let snakeHead = this.state.snakeCells[0];
+    let foodPosition = this.state.foodPosition;
+    if (snakeHead[0] === foodPosition[0] && snakeHead[1] === foodPosition[1] ) {
+      return true;
+    }
+    return false;
   }
+
   /**
    * Checks if snake has collided with wall or itself
    * @param {*} snakeCells the snake cells which are on the board(The snake body) 
    */
   hasCollision(snakeCells) {
     let snakeHead = snakeCells[0]; 
-    if ( snakeHead[0] > 15 || snakeHead[1] > 15) { // went outside of the boundary
+    if ( snakeHead[0] > 15 || snakeHead[1] > 15 || snakeHead[0] < 0 || snakeHead[1] < 0 ) { // went outside of the boundary
       return true;
     }
     let snakeCellsExceptHead = snakeCells.slice(1);
@@ -113,7 +142,7 @@ class App extends Component {
   render() {
     return (
       <div className="App Center" >
-      <Board snakeCells= {this.state.snakeCells}></Board>
+      <Board snakeCells= {this.state.snakeCells} foodPosition={this.state.foodPosition}></Board>
       <button onClick={this.startGame}>Start Playing With A Snake.</button>
       <button onClick={this.pauseGame}>Pause</button>
       </div>);
@@ -123,18 +152,30 @@ class App extends Component {
 class Board extends Component{
   render() {
       let rows = [];
-      for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-          let id = `${i}${j}`
-          let isSnakeCell = this.props.snakeCells.find((element) => {
+      let snakeCells = this.props.snakeCells;
+      let foodPosition = this.props.foodPosition;
+      GRID.forEach((element) => {
+          let i = element[0];
+          let j = element[1];
+          // checking if the cell belongs from snake body
+          let isSnakeCell = snakeCells.find((element) => {
             return (element[0] === i && element[1] === j)
           })
-          rows.push( <div id = {id}
-            className = {
-              'Cell ' + (isSnakeCell ? 'SankeCell' : '')
-            } > </div>);
+          let isFoodCell = false;
+          console.log(element[0], element[1]);
+          if(!isSnakeCell){
+            console.log("I am getting inside")
+            isFoodCell = (foodPosition[0] === i && foodPosition[1] === j);
+            console.log('isFoodCell', isFoodCell);
           }
-        }
+          
+          if (isFoodCell) {
+            rows.push(<div className='Food-cell'> <span className='Food'>&#127828;</span> </div>)
+          } else {
+            rows.push( <div className = { 'Cell ' + (isSnakeCell ? 'SankeCell' : '') } > </div>);
+          }
+          
+        })
         return ( <div > {rows} </div>)
         }
     }
