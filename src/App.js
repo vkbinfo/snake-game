1c4b225c780cf904d2a6672e5c25b14b77859bce
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-01 08:39:38 +0530

    handled closing of the login form and also handled the actions after login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import axios from 'axios';
 
 // importing components
 import Game from './components/game/game';
@@ -11,25 +12,52 @@ import LogIn from './components/login/login';
 class App extends Component {
   state = {
     isLoggedIn: false,
-    renderLoginForm: true,
+    renderLoginForm: false,
+    username: '',
   }
   constructor(props) {
     super(props)
     this.renderLoginForm = this.renderLoginForm.bind(this);
+    this.closeLoginForm = this.closeLoginForm.bind(this);
+    this.actionAfterLogIn = this.actionAfterLogIn.bind(this);
   }
+
   renderLoginForm() {
-    this.setState({renderLoginForm: true})
+    if(this.state.isLoggedIn){
+      axios.post('https://evening-oasis-31820.herokuapp.com/user/delete/token', {
+            
+          })
+          .then((response) => {
+            
+          })
+          .catch((error) => {
+            alert('Something went wrong while logging out.')
+            console.error('login error', error);
+          });
+    } else {
+      this.setState({renderLoginForm: true});
+    }
+    
   }
+
+  closeLoginForm(){
+    this.setState({renderLoginForm: false})
+  }
+
+  actionAfterLogIn(username){
+    this.setState({isLoggedIn: true, renderLoginForm: false, username})
+  }
+
   render() {
     const isLoggedIn = this.state.isLoggedIn;
     const renderLoginForm =  this.state.renderLoginForm;
     let button = 'LogIn';
     let logInFormComponent = null;
     if (isLoggedIn) {
-      button = 'Welcome back, Tommy{replace it with original name}'
+      button = `Welcome back, ${this.state.username}! click to logout.`
     }
     if (renderLoginForm) {
-      logInFormComponent = <LogIn></LogIn>
+      logInFormComponent = <LogIn closeLoginForm={this.closeLoginForm} actionAfterLogIn={this.actionAfterLogIn}></LogIn>
     }
     return (
       <div className='Container'>
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import './login.css'
+import axios from 'axios';
+import './login.css';
+const jwt = require('jsonwebtoken');
+
 class LogIn extends Component{
     state = {
         loginForm: true,
@@ -10,6 +13,7 @@ class LogIn extends Component{
         super(props)
         this.renderSignUpForm = this.renderSignUpForm.bind(this);
         this.loginSubmit = this.loginSubmit.bind(this);
+        this.signUpSubmit = this.signUpSubmit.bind(this);
     }
 
     renderSignUpForm() {
@@ -18,10 +22,40 @@ class LogIn extends Component{
 
     loginSubmit() {
         this.setState({spinner: true});
+        let username = this.refs.username.value;
+        let password = this.refs.password.value;
+        axios.post('https://evening-oasis-31820.herokuapp.com/user/login', {
+            username,
+            password 
+          })
+          .then((response) => {
+            const tokenData = jwt.decode(response.data['x-auth']);
+            this.setState({spinner: false});
+            this.props.actionAfterLogIn(tokenData.username)
+          })
+          .catch((error) => {
+            alert('Either username or password is wrong.')
+            console.error('login error', error);
+            this.setState({spinner: false});
+          });
     }
 
     signUpSubmit() {
-        alert('It submitted');
+        this.setState({spinner: true});
+        axios.post('https://evening-oasis-31820.herokuapp.com/user/new', {
+            username: this.refs.newUsername.value,
+            password: this.refs.newPassword.value
+          })
+          .then((response) => {
+            const tokenData = jwt.decode(response.data['x-auth']);
+            this.setState({spinner: false});
+            this.props.actionAfterLogIn(tokenData.username)
+          })
+          .catch((error) => {
+            alert('Either your username is not unique or your password length is less than 3.')
+            console.error('login error', error);
+            this.setState({spinner: false});
+          });
     }
 
     render() {
@@ -30,9 +64,9 @@ class LogIn extends Component{
             formToRender = (<div>
                 <form onSubmit={this.loginSubmit}>
                     <label>username</label><br></br>
-                    <input name="username" type="text"  /><br></br>
+                    <input  ref="username" name="username" type="text"  /><br></br>
                     <label >password</label><br></br>
-                    <input name="password" type="password"/><br></br>
+                    <input ref="password" name="password" type="password"/><br></br>
                         <input type='submit' value="LogIn"/><br></br>
                 </form>
                         <button onClick={this.renderSignUpForm}>Sign Up</button>
@@ -41,26 +75,26 @@ class LogIn extends Component{
             formToRender = (<div>
                 <form onSubmit={this.signUpSubmit}>
                     <label>username</label><br></br>
-                    <input name="Enter Username" type="text"  /><br></br>
+                    <input ref="newUsername" name="Enter Username" type="text"  /><br></br>
                     <label >password</label><br></br>
-                    <input name="Enter Password" type="password"/><br></br>
-                        <input type='submit' value="LogIn"/><br></br>
+                    <input ref="newPassword" name="Enter Password" type="password"/><br></br>
+                        <input type='submit' value="Sign Up"/><br></br>
                 </form>
                         <button>Back To LogIn</button>
                 </div>)
         }
         let renderElementInFormContainer = formToRender;
         if (this.state.spinner) {
-            renderElementInFormContainer = (<div class="Spinner">
+            renderElementInFormContainer = (<div className="Spinner">
             <div></div>
             <div></div>
             <div></div>
             <div></div>
-            <p>Shhh!!! talking with cloud.</p>
+            <p>Shhh!!! I am talking with Cloud.</p>
           </div>)
         }
         return (<div className="Login-container ">
-       <p className='Cross-sign'>&#10005;</p>
+       <p className='Cross-sign' onClick={this.props.closeLoginForm}>Close &#10005;</p>
        <h3>Welcome To Snake Land</h3>
        {renderElementInFormContainer}
       </div>)
